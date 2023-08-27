ff9b5ac78d6da3c1e1733a8cfff0a467589f77e7
KAMAH <limitlesskds@gmail.com>
2023-08-27 11:09:34 +0100

    Added a function to get a database collection from the mongo database

--- src/store/db.service.ts
+++ src/store/db.service.ts
@@ -3,5 +3,10 @@ import { Db } from "mongodb";
 
 @Injectable()
 export class DatabaseService {
+
   constructor(@Inject('MONGO_CONNECTION') private db:Db) {}
+
+  public getDbCollection(collectionName: string) {
+    return this.db.collection(collectionName)
+  }
 }
