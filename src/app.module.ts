08a820ae34877e335a6a5b83de4d7b7b9dcd15c9
KAMAH <limitlesskds@gmail.com>
2023-08-29 00:46:09 +0100

    Made custom error class

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MongoModule } from './store/db.module';
 import { DatabaseService } from './store/db.service';
+import { ResponseData } from './logger.service';
 
 
 @Module({
@@ -11,6 +12,6 @@ import { DatabaseService } from './store/db.service';
     ConfigModule.forRoot(), 
     MongoModule.registerAsync()],
   controllers: [AppController],
-  providers: [AppService, DatabaseService],
+  providers: [AppService, DatabaseService, ResponseData],
 })
 export class AppModule {}
--- src/logger.service.ts
+++ src/logger.service.ts
@@ -2,8 +2,24 @@ import { HttpException, HttpStatus, Logger, Injectable } from "@nestjs/common";
 
 @Injectable()
 export class ResponseData {
-  responseCode: number
-  responseMessage: String
-  errorMessage: String
   
+  private static responseObject: Record<string,any>
+
+  /**
+   * 
+   * @code Valid HTTP status code  
+   * @message Custom message for the client
+   * @payload Respnse data to send to client
+   * @err Error in request
+   */
+
+  static log(code: HttpStatus, message: string, 
+    payload?:string, err?: HttpException): void 
+  {
+    this.responseObject = { code, message, payload, err }
+    return Logger.log(this.responseObject)
+  }
 }
+
+
+
