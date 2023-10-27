64f8b0d3d683ed5242cf2910b50047b248c028be
KAMAH <limitlesskds@gmail.com>
2023-10-27 17:44:58 +0100

    Fixed error in login controller. Added skip auth to login

--- src/auth/auth.controllers.ts
+++ src/auth/auth.controllers.ts
@@ -2,15 +2,16 @@ import { Controller, Post, Req, Res, UseGuards } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { Response } from "express"
 import { LocalAuthGuard } from "./local.strategy";
+import { SkipAuth } from "./jwt.strategy";
 
 @Controller('api/v1/auth')
 export class AuthControllers {
   constructor(private readonly authService: AuthService) {}
 
+  @SkipAuth()
   @UseGuards(LocalAuthGuard)
   @Post('/login')
   async login(@Req() req, @Res() res: Response) {
-    console.log(req)
     return res.status(200).json(await this.authService.login(req.user))
   }
 }
