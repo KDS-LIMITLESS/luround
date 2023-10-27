a383769f3a611930ebe9e5e678dd30160d97dbbe
KAMAH <limitlesskds@gmail.com>
2023-10-27 23:34:31 +0100

    Implemented custom validation decorator for the certificates array

--- src/profileManager/profile.dto.ts
+++ src/profileManager/profile.dto.ts
@@ -1,6 +1,59 @@
-import { IsDate, IsEmail, IsNotEmpty, IsNumber } from "class-validator";
+import { Contains, IsArray, IsDate, IsEmail, IsNotEmpty, IsNumber, ValidationArguments, 
+  ValidationOptions, 
+  ValidatorConstraint, ValidatorConstraintInterface, registerDecorator 
+} from "class-validator";
 import { PartialType, PickType } from '@nestjs/mapped-types'
+import { BadRequestException } from "@nestjs/common";
 
+
+@ValidatorConstraint({async: true})
+export class IsCertificateConstraint implements ValidatorConstraintInterface {
+  validate(certificates: any, args: ValidationArguments): boolean {
+    try {
+      if (Array.isArray(certificates)) {
+
+        let  certificateName, issuingOrganization, issueDate, validateCertificate, date;
+        certificates.forEach(function(certObj) {
+          certificateName = Object.getOwnPropertyDescriptor(certObj, 'certificateName').value
+          issuingOrganization = Object.getOwnPropertyDescriptor(certObj, 'issuingOrganization').value
+          issueDate = Object.getOwnPropertyDescriptor(certObj, 'issueDate').value
+          
+          date = new Date(issueDate)
+          if(date.toString() === "Invalid Date") throw Error("issueDate must be a date object. YYYY-MM ")
+
+          if((certificateName && issuingOrganization && issueDate) !== '' &&
+            (certificateName && issuingOrganization && issueDate) !== null &&
+            (certificateName && issuingOrganization && issueDate) !==undefined) 
+          { validateCertificate = true } 
+          else { 
+            validateCertificate = false; 
+            throw Error('certificateName and issuingOrganization fields must not be empty') 
+          }
+        })
+        return validateCertificate
+      }
+      throw Error("Certificates must be an array")
+    }
+    catch (e: any) {
+      throw new BadRequestException({
+        message: e.message
+      })
+    }
+    
+  }
+}
+
+export function IsCertificate(validtionOptions?: ValidationOptions) {
+  return function(object: Object, propertyName: string) {
+    registerDecorator({
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validtionOptions,
+      constraints: [],
+      validator: IsCertificateConstraint,
+    });
+  }
+}
 
 export class userProfileMainDto{
   @IsNotEmpty()
@@ -22,7 +75,7 @@ export class userProfileMainDto{
   @IsNotEmpty()
   about: string
   
-  @IsNotEmpty()
+  @IsCertificate()
   certificates: Array<{[key: string]: any}>
 
   @IsNotEmpty()
@@ -48,8 +101,7 @@ export class CertificateValidationDto {
   @IsNotEmpty() @IsDate()
   issueDate: Date
 
-  @IsNumber({allowNaN: false})
-  certificateID: number
+
 }
 
 
