5d4a9d44afb95cb60dca4b78510fcf3e7031d6bd
KAMAH <limitlesskds@gmail.com>
2024-01-20 19:49:12 +0100

    Set cors origin to luround domain

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({origin: "https://luround.com"})
+  app.enableCors({origin: "*"})
   app.useGlobalPipes(new ValidationPipe())
   await app.listen(process.env.PORT);
 }
