c665038b6f36969dbae81985e04a59e7916fe07a
KAMAH <limitlesskds@gmail.com>
2024-03-05 07:28:16 +0100

    Added try cath blocks to verify payment api to catch potential errors

--- src/payments/paystack.sevices.ts
+++ src/payments/paystack.sevices.ts
@@ -50,17 +50,22 @@ export class PaymentsAPI {
         'Content-Type': 'application/json'
       }
     }
-    let request : any= await PaymentsAPI.makeRequest(transaction_ref, options)
-    if (request.data.status === 'success') {
-      let date = new Date(`${request.data.paid_at}`)
-      await this.databaseManager.updateDocument(this._udb, userId, {account_status: "ACTIVE", payment_details: {
-        start_date: request.data.paid_at,
-        expiry_date: new Date(date.getTime() + 30 * 24 * 60 * 60 * 1000),
-        authourization: request.data.authorization,
-      }})
-      return request.data.status
-    } 
-    throw new BadRequestException({message: request.data.status})
+    let request: any = await PaymentsAPI.makeRequest(transaction_ref, options)
+    try {
+      if (request.data.status === 'success') {
+        let date = new Date(`${request.data.paid_at}`)
+        await this.databaseManager.updateDocument(this._udb, userId, {account_status: "ACTIVE", payment_details: {
+          start_date: request.data.paid_at,
+          expiry_date: new Date(date.getTime() + 30 * 24 * 60 * 60 * 1000),
+          authourization: request.data.authorization,
+        }})
+        return request.data.status
+      } 
+      throw new BadRequestException({message: request.data.status})
+    } catch (err: any) {
+      throw new BadRequestException({message: request.message || err.message })
+    }
+    
   }
 
   static async create_yearly_payment_plan() {
