c74def47be182d4e100525c6e2fc6b5fd0293279
KAMAH <limitlesskds@gmail.com>
2024-06-26 17:45:32 +0100

    Added setTimeOut fuction to delay booking payment verification till the booking arrives in DB

--- src/payments/paystack.controllers.ts
+++ src/payments/paystack.controllers.ts
@@ -61,11 +61,12 @@ export class Payments {
         console.log("Status:", "Transaction Recorded Successfully!")
       }
       if (eventData.event === 'charge.success') {
-        let verify_booking = await this.paymentManager.verifyBookingPayment(eventData.data.reference.toString(), eventData.data.amount)
-        console.log("Verified Booking:", verify_booking)
-        await this.bookingService.confirm_booking(verify_booking.booking_id)
-
-        // this.paymentManager.verifyBookingPayment()
+        // SET TIMEOUT TO ALLOW FOR THE BOOKING TO REGISTER FIRST
+        setTimeout(async () => {
+          let verify_booking = await this.paymentManager.verifyBookingPayment(eventData.data.reference.toString(), eventData.data.amount)
+          console.log("Verified Booking:", verify_booking)
+          await this.bookingService.confirm_booking(verify_booking.booking_id)
+        }, 3000)
       }
       return res.sendStatus(200)
     }
