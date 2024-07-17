0d06a33f647ed07b99b6fd089fcd1def59daf93e
KAMAH <limitlesskds@gmail.com>
2024-07-17 23:13:03 +0100

    Bug fix

--- src/feedback/feedback.services.ts
+++ src/feedback/feedback.services.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
 import ResponseMessages from "../messageConstants.js";
 import { DatabaseService } from "../store/db.service.js";
 import { SendFeedBackEmail } from "../utils/mail.services.js";
@@ -11,10 +11,16 @@ export class FeedBackService {
   constructor(private databaseManager: DatabaseService) {}
 
   async record_feedback(user: any, data: any){
+    try {
     const {displayName, userId, email} = user
     await this.databaseManager.create(this._fdb, {userId, displayName, feed_back: data})
     await SendFeedBackEmail(email, displayName.split(' ')[0], data.subject, data.description)
     return ResponseMessages.FeedBackRecorded
+    } catch(err: any) {
+      console.log(err)
+      throw new BadRequestException({message: err.message})
+    }
+    
   }
 
   async get_feedBacks() {
