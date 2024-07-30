b935cd58cdd4f3ccb65a9e78484d1c29e696ba71
KAMAH <limitlesskds@gmail.com>
2024-07-30 10:32:53 +0100

    Added exception filters

--- main.ts
+++ main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './src/app.module.js';
 import { ValidationPipe } from '@nestjs/common';
 import { loadCronJobs } from './src/utils/mail.services.js';
+import { FilterExceptions } from './src/utils/exception-filter.js'
 
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({origin: "*"})
   app.useGlobalPipes(new ValidationPipe())
+  app.useGlobalFilters(new FilterExceptions())
   await app.listen(process.env.PORT);
   await loadCronJobs()
 }
